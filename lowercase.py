d11998fa9b8a912cf1cbddd4c22058059610752d
ric-rhee <rheer@purdue.edu>
2021-03-24 23:20:28 -0400

    testing

--- lowercase.py
+++ lowercase.py
@@ -1,7 +1,7 @@
 string = "ABCDEFG"
 
 def convert_to_lower(string_to_convert):
-    return string_to_convert.upper()
+    return string_to_convert.lower()
 
 def test_convert_to_lower():
     assert all(97 <= ASCII <= 122 for ASCII in [ord(ele) for ele in string.lower()])
