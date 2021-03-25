a86d9d76f086bba83400fd94f8e0aeeeb17e61d2
The-Indian-Chinna <achinnak@purdue.edu>
2021-03-25 15:57:47 -0400

    Hey

--- lowercase.py
+++ lowercase.py
@@ -4,4 +4,4 @@ def convert_to_lower(string_to_convert):
     return string_to_convert.upper()
 
 def test_convert_to_lower():
-    assert all(97 <= ASCII <= 122 for ASCII in [ord(ele) for ele in string.lower()])
+    assert all(97 <= ASCII <= 122 for ASCII in [ord(ele) for ele in convert_to_lower(string)])
